3f01b9a31d5ef8de5386ef05ff931c0bf8f69719
JunSeokCheon <goosw775@naver.com>
2022-02-15 14:53:40 +0900

    Elice-backend

---
+++ 2-15이론/processing.js
@@ -0,0 +1,14 @@
+// import {kor, eng, mat} from './grade.js'
+// import {grade} from './grade.js'
+
+// console.log(grade)
+// console.log(grade.kor)
+// console.log(grade.eng)
+// console.log(grade.mat)
+
+const grade = require('./grade.js')
+console.log(grade)
+console.log(grade.kor)
+console.log(grade.mat)
+console.log(grade.eng)
+console.log(grade.myfoo())
