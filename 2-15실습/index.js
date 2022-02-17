d5ad65874faa62cb77b42ed53fc415f4ec1837d9
JunSeokCheon <goosw775@naver.com>
2022-02-17 10:54:13 +0900

    elice backend

--- 2-15실습/index.js
+++ 2-15실습/index.js
@@ -7,40 +7,40 @@ const app = express();
 console.log(express)
 const arr = [];
 
-// app.get("/", (req, res) => {
-//     fs.readFile("./index.html", "utf-8", (err, data) => {
-//         res.send(data)
-//     })
-// });
+app.get("/", (req, res) => {
+    fs.readFile("./index.html", "utf-8", (err, data) => {
+        res.send(data)
+    })
+});
 
-// app.get("/arr", (req, res) => {
-//   res.send({
-//     arr: arr,
-//   });
-// });
+app.get("/arr", (req, res) => {
+  res.send({
+    arr: arr,
+  });
+});
 
-// app.post("/arr", (req, res) => {
-//   arr.push("new Data");
-//   res.send({
-//     status: "succ",
-//   });
-// });
+app.post("/arr", (req, res) => {
+  arr.push("new Data");
+  res.send({
+    status: "succ",
+  });
+});
 
-// app.put("/arr", (req, res) => {
-//   arr[0] = "Update";
-//   res.send({
-//     status: "succ",
-//   });
-// });
+app.put("/arr", (req, res) => {
+  arr[0] = "Update";
+  res.send({
+    status: "succ",
+  });
+});
 
-// app.delete("/arr", (req, res) => {
-//   arr.pop();
-//   res.send({
-//     status: "succ",
-//   });
-// });
+app.delete("/arr", (req, res) => {
+  arr.pop();
+  res.send({
+    status: "succ",
+  });
+});
 
-// app.listen(3000, () => console.log("서버 켜짐 3000 포트"));
+app.listen(3000, () => console.log("서버 켜짐 3000 포트"));
 
 
 // function onRequest(req, res) {
